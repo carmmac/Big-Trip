cfb8390cfb6b4147ed9c92c7a7a5ea8bf311c0be
bremxx <petr.kirienko@gmail.com>
2020-11-21 00:47:24 +0300

    модули для компонентов интерфейса, импорт и отрисовка

---
+++ src/main.js
@@ -0,0 +1,39 @@
+import {info} from './view/info.js';
+import {menu} from './view/menu.js';
+import {filters} from './view/filters.js';
+import {statistics} from './view/statistics.js';
+import {list} from './view/list.js';
+import {listEmpty} from './view/list-empty.js';
+import {listSort} from './view/list-sort.js';
+import {edit} from './view/edit.js';
+import {newPoint} from './view/new-point.js';
+import {newPointWithoutOffers} from './view/new-point-without-offers.js';
+import {newPointWithoutDestinations} from './view/new-point-without-destination.js';
+import {listFilter} from './view/list-filter.js';
+
+const render = function (container, template, position) {
+  container.insertAdjacentHTML(position, template);
+};
+
+const tripHeaderElement = document.querySelector(`.trip-main`);
+render(tripHeaderElement, info(), `afterbegin`);
+
+const tripMenuElement = tripHeaderElement.querySelector(`.trip-controls`);
+render(tripMenuElement, menu(), `afterbegin`);
+render(tripMenuElement, filters(), `beforeend`);
+
+const siteMainElement = document.querySelector(`.page-main .page-body__container`);
+render(siteMainElement, statistics(), `beforeend`);
+
+const tripEventsElement = siteMainElement.querySelector(`.trip-events`);
+render(tripEventsElement, listEmpty(), `afterbegin`);
+render(tripEventsElement, listSort(), `afterbegin`);
+render(tripEventsElement, list(), `beforeend`);
+render(tripEventsElement, listFilter(), `beforeend`);
+
+const tripEventsListElement = tripEventsElement.querySelector(`.trip-events__list`);
+render(tripEventsListElement, edit(), `afterbegin`);
+render(tripEventsListElement, newPointWithoutDestinations(), `afterbegin`);
+render(tripEventsListElement, newPointWithoutOffers(), `afterbegin`);
+render(tripEventsListElement, newPoint(), `afterbegin`);
+
