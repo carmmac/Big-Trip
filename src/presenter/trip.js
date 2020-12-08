bd0f0288d50f6e680052386b26bc99b0fd60a4d2
bremxx <petr.kirienko@gmail.com>
2020-12-08 13:32:23 +0300

    наполнение презентера маршрута функционалом

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -5,7 +5,7 @@ import SortView from '../view/sort.js';
 import ListView from '../view/list.js';
 import EmptyListView from '../view/list-empty.js';
 import HeadingView from '../view/heading.js';
-import {render, RenderPosition} from '../utils/utils-render.js';
+import {render, RenderPosition, replace} from '../utils/utils-render.js';
 import {checkEmptyData} from '../utils/utils-common.js';
 
 export default class Trip {
@@ -17,12 +17,68 @@ export default class Trip {
     this._emptyListComponent = new EmptyListView();
     this._headingComponent = new HeadingView();
   }
+
   init(tripEvents) {
     this._tripEvents = tripEvents.slice();
+    render(this._tripContainer, this._tripBoardComponent, RenderPosition.AFTERBEGIN);
+
+    this._renderTrip();
   }
-  renderSort() {}
-  renderEvent() {}
-  renderList() {}
-  renderEmptyList() {}
-  renderTrip() {}
+
+  _renderTrip() {
+    if (checkEmptyData(this._tripEvents)) {
+      this._renderEmptyList();
+      return;
+    }
+    this._renderSort();
+    this._renderList();
+    this._renderEvents();
+  }
+
+  _renderSort() {
+    render(this._tripBoardComponent, this._sortComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderList() {
+    render(this._tripBoardComponent, this._listComponent, RenderPosition.BEFOREEND);
+  }
+
+  _renderEvents() {
+    this._tripEvents.forEach((event) => this._renderEvent(event));
+  }
+
+  _renderEvent(event) {
+    const eventComponent = new EventView(event);
+    const eventEditComponent = new EventEditView(event);
+    const replaceCardToForm = () => {
+      replace(eventEditComponent, eventComponent);
+    };
+    const replaceFormToCard = () => {
+      replace(eventComponent, eventEditComponent);
+    };
+    render(this._listComponent, eventComponent, RenderPosition.BEFOREEND);
+    const eventFormOpenHandler = () => {
+      replaceCardToForm();
+      document.addEventListener(`keydown`, EscPressHandler);
+      eventEditComponent.setFormCloseHandler(eventFormCloseHandler);
+      eventEditComponent.setFormSubmitHandler(eventFormCloseHandler);
+    };
+    const eventFormCloseHandler = () => {
+      replaceFormToCard();
+      document.removeEventListener(`keydown`, EscPressHandler);
+      eventEditComponent.removeFormCloseHandler();
+      eventEditComponent.removeFormSubmitHandler();
+    };
+    const EscPressHandler = (evt) => {
+      if (evt.key === `Escape` || evt.key === `Esc`) {
+        evt.preventDefault();
+        eventFormCloseHandler();
+      }
+    };
+    eventComponent.setFormOpenHandler(eventFormOpenHandler);
+  }
+
+  _renderEmptyList() {
+    render(this._tripBoardComponent, this._emptyListComponent, RenderPosition.BEFOREEND);
+  }
 }
