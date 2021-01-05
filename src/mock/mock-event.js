eef414822557f2e5effaaf16c1dd9d5fd6c3ab3d
bremxx <petr.kirienko@gmail.com>
2021-01-05 16:01:37 +0300

    рефакторинг моковой функции генерации id
    - parseInt() заменен на Math.floor()

--- src/mock/mock-event.js
+++ src/mock/mock-event.js
@@ -5,7 +5,7 @@ import dayjs from 'dayjs';
 import objectSupport from 'dayjs/plugin/objectSupport';
 dayjs.extend(objectSupport);
 
-export const generateId = () => Date.now() + parseInt(Math.random() * 10000, 10);
+export const generateId = () => Date.now() + Math.floor(Math.random() * 10000);
 
 const infoTest = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 export const eventTypes = [
